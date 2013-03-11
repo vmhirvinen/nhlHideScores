59c35164399a91bd24850d27ea53d7333f9712d2
VM Hirvinen <vm_hirvinen@hotmail.com>
2013-03-11 22:52:49 +0200

    handle live and ended games

--- hideNhlScores/nhlHideScores.js
+++ hideNhlScores/nhlHideScores.js
@@ -1,12 +1,28 @@
-document.getElementById("hsbGames").addEventListener("DOMSubtreeModified", function(evt) {  
-	var elem = evt.target;	
+function replaceValues(v) {
+	return v.replace(/OT/gi, "").replace(/SO/gi, "");
+}
+
+// when some games are still live, nodes are modified after insertion
+document.addEventListener("DOMSubtreeModified", function(event) {  
+	var elem = event.target;		
 	if (elem && elem.nodeType == 3) {		
-		var value = elem.nodeValue;
+		var value = elem.nodeValue;		
 		if (value && (value.indexOf("OT") > -1 || value.indexOf("SO") > -1)) {
 			var parent = elem.parentElement;
 			if (parent) {
-				parent.innerHTML = value.replace(/OT/gi, "").replace(/SO/gi, "");
+				parent.innerHTML = replaceValues(value);
 			}
 		}
 	}
+});
+
+// event handler to remove OT/SO markups on initial DOM insertion
+document.addEventListener("DOMNodeInserted", function(event) {
+	var elem = event.target;
+	if (elem.tagName == 'DIV' && elem.className == 'gameItem') {		
+		var nodeList = document.querySelectorAll(".offvalue");
+		for (var i = 0, length = nodeList.length; i < length; i++) {     		
+			nodeList[i].innerHTML = replaceValues(nodeList[i].innerHTML);
+		}
+	}               
 });
