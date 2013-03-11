8ffa6ff70cf059162aa11101989b4352b5ae4605
vmhirvinen <hirvinen@gmail.com>
2013-03-11 12:08:48 +0200

    Update nhlHideScores.js

--- hideNhlScores/nhlHideScores.js
+++ hideNhlScores/nhlHideScores.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMNodeInserted", function(event) {
 	var elem = event.target;
 
 	 if (elem.tagName == 'DIV' && elem.className == 'gameItem') {
-		console.log("The node '" + elem.tagName + " (" + elem.className + ")' has been added to an document.";
+		console.log("The node '" + elem.tagName + " (" + elem.className + ")' has been added to an document.");
 
 		var nodeList = document.querySelectorAll(".offvalue");
 		  for (var i = 0, length = nodeList.length; i < length; i++) {
