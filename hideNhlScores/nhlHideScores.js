96af0ab89eb5eae2d72e980961136968acadc871
vmhirvinen <hirvinen@gmail.com>
2013-03-11 13:13:41 +0200

    Update nhlHideScores.js
    DOMSubtreeModified approach

--- hideNhlScores/nhlHideScores.js
+++ hideNhlScores/nhlHideScores.js
@@ -1,17 +1,12 @@
-document.addEventListener("DOMNodeInserted", function(event) {
-	var elem = event.target;
-
-	 if (elem.tagName == 'DIV' && elem.className == 'gameItem') {
-		console.log("The node '" + elem.tagName + " (" + elem.className + ")' has been added to an document.");
-
-		var nodeList = document.querySelectorAll(".offvalue");
-		  for (var i = 0, length = nodeList.length; i < length; i++) {
-     		var v = nodeList[i].innerHTML;
-     		nodeList[i].innerHTML = v.replace(/OT/gi, "").replace(/SO/gi, "");
-		  }
-
-	 }
-     
-            
-
+document.getElementById("hsbGames").addEventListener("DOMSubtreeModified", function(evt) {  
+	var elem = evt.target;	
+	if (elem && elem.nodeType == 3) {		
+		var value = elem.nodeValue;
+		if (value && (value.indexOf("OT") > -1 || value.indexOf("SO") > -1)) {
+			var parent = elem.parentElement;
+			if (parent) {
+				parent.innerHTML = value.replace(/OT/gi, "").replace(/SO/gi, "");
+			}
+		}
+	}
 });
