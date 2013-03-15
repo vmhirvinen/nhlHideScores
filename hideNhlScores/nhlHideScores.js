e444bf3c99db4db5ae90d08eb12bc5c2eff93dd6
VM Hirvinen <vm_hirvinen@hotmail.com>
2013-03-15 07:50:26 +0200

    fixed hiding live scores

--- hideNhlScores/nhlHideScores.js
+++ hideNhlScores/nhlHideScores.js
@@ -2,11 +2,22 @@ function replaceValues(v) {
 	return v.replace(/OT/gi, "").replace(/SO/gi, "");
 }
 
+function replaceBySelector(sel) {
+	var nodeList = document.querySelectorAll(sel);
+	for (var i = 0, length = nodeList.length; i < length; i++) {     		
+		nodeList[i].innerHTML = replaceValues(nodeList[i].innerHTML);
+	}
+}
+
 // when some games are still live, nodes are modified after insertion
 document.addEventListener("DOMSubtreeModified", function(event) {  
-	var elem = event.target;		
+	var elem = event.target;
+	if (elem.className == 'bsc final') {
+		replaceBySelector(".offvalue");
+	}
+	
 	if (elem && elem.nodeType == 3) {		
-		var value = elem.nodeValue;		
+		var value = elem.nodeValue;				
 		if (value && (value.indexOf("OT") > -1 || value.indexOf("SO") > -1)) {
 			var parent = elem.parentElement;
 			if (parent) {
@@ -19,10 +30,7 @@ document.addEventListener("DOMSubtreeModified", function(event) {
 // event handler to remove OT/SO markups on initial DOM insertion
 document.addEventListener("DOMNodeInserted", function(event) {
 	var elem = event.target;
-	if (elem.tagName == 'DIV' && elem.className == 'gameItem') {		
-		var nodeList = document.querySelectorAll(".offvalue");
-		for (var i = 0, length = nodeList.length; i < length; i++) {     		
-			nodeList[i].innerHTML = replaceValues(nodeList[i].innerHTML);
-		}
+	if (elem.tagName == 'DIV' && elem.className == 'gameItem') {				
+		replaceBySelector(".offvalue");
 	}               
 });
